9ff0c5fa4d7c309a50db85004b837f1509964927
Zachary Puls <zach@zachpuls.com>
2016-05-19 19:10:28 +0000

    Fix spacing.

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,6 +4,7 @@
 repeat:
 	jmp repeat
 	
-times 510-($ - $$) db 0 	; Pad the file to 512 bytes.
+times 510-($ - $$) db 0      ; Pad the file to 512 bytes.
 
-dw 0xaa55					; Boot sector magic number.
+
+dw 0xaa55                    ; Boot sector magic number.
